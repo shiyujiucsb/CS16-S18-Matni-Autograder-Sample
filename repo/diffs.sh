916d2ba3a8ad5b88c0c6fb2a8f87528e34293de2
Shiyu Ji <shiyu@cs.ucsb.edu>
2019-10-06 10:30:35 -0700

    Add sample files.

---
+++ repo/diffs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# @test{"stdout":10}
+./hello
+
+# @test{"stdout":10, "output-visibility":"hidden"}
+./hello
+
+# @test{"stdout":10}
+./hello < sample.stdin
+
+# @test{"stdout":10}
+echo "sample input" | ./hello
+
